3dc451ea2aee5b03c9dd883d90bb8009343ebb26
GregoireDuhem <gregoire.duhem@epitech.eu>
2024-03-19 11:52:33 +0100

    [+] add animation

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,305 +1,257 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-import { ChevronLeft, ChevronRight } from "lucide-react";
+import React, { useState, useEffect } from "react";
+import styled, { css, keyframes } from "styled-components";
 import { PixelStreamingWrapper } from "./pixelWrapper";
+import Lottie from "react-lottie";
+import animationData from "/src/assets/eagle.json";
+import loader from "/src/assets/loaderTxt.json";
+
+const clickAnimation = keyframes`
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(0.95);
+  }
+  100% {
+    transform: scale(1);
+  }
+`;
+
+const LoadingWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: var(--whiteColor);
+  display: grid;
+  justify-items: center;
+  align-content: center;
+  z-index: 1000;
+`;
+
+const ToggleButton = styled.button`
+  margin: var(--xs-sizing) var(--s-sizing);
+  padding: var(--s-sizing);
+  background-color: var(--blackGreyColor);
+  color: var(--whiteColor);
+  border: 1px solid var(--whiteColor);
+  cursor: pointer;
+  font-size: var(--m-sizing);
+  border-radius: 10px;
+  transition: background-color 0.3s ease, color 0.3s ease;
+
+  ${(props) =>
+    props.active &&
+    css`
+      background-color: var(--whiteColor);
+      color: var(--blackGreyColor);
+    `}
+
+  &:hover {
+    background-color: var(--whiteColor);
+    color: var(--blackGreyColor);
+  }
+
+  &.animatedOnClick {
+    animation: ${clickAnimation} 0.2s ease;
+  }
+`;
+
+const Snackbar = styled.div`
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  background-color: var(--whiteColor);
+  color: var(--blackGreyColor);
+  padding: var(--m-sizing);
+  border-radius: var(--xs-sizing);
+  z-index: 1050;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const StyledHome = styled.div`
   width: 100vw;
   height: 100vh;
   background-color: var(--blackColor);
   display: flex;
+  flex-direction: column;
+  justify-content: space-between;
   align-items: center;
-  justify-content: flex-end;
-  .streamingSide {
-    width: 100%;
-    height: 100%;
-    position: relative;
-  }
-  .toggleButton {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background-color: #333333;
-    width: 20px;
-    height: 80px;
-    position: relative;
-    cursor: pointer;
-    border-top-left-radius: 15px;
-    border-bottom-left-radius: 15px;
-    &:hover {
-      background-color: #292929;
-    }
-  }
-  .rightSide {
+  position: relative;
+
+  .buttonContainer {
     position: absolute;
-    top: 0;
-    right: 0;
-    z-index: 10;
-    // When rightSide is not open
-    transform: translateX(${(props) => (props.isRightSideOpen ? "0" : "100%")});
-    width: ${(props) => (props.isRightSideOpen ? "20%" : "0")};
-    transition: width 0.3s;
-    overflow: hidden;
-    height: 100%;
-    background-color: var(--blackGreyColor);
+    top: 95%;
+    left: 50%;
+    transform: translate(-50%, -50%);
     display: flex;
-    flex-direction: column;
-    align-items: center;
-
-    .leftPanel {
-      padding-top: var(--xl-sizing);
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: 18%;
-      height: 100%;
-      background-color: #333333;
-      button {
-        font-size: 12px;
-        border-radius: 0;
-        width: 100%;
-        height: 10%;
-        &.active {
-          background-color: #000;
-        }
-      }
-    }
-    .colorList {
-      flex-direction: column;
-      align-items: center;
-      width: 100%;
-      list-style: none;
-      padding: 0;
-      display: flex;
-      justify-content: center;
-      margin-top: var(--xl-sizing);
-      margin-left: var(--l-sizing);
-
-      li {
-        width: 100px;
-        height: 100px;
-        margin: var(--xs-sizing);
-        border-radius: var(--xs-sizing);
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-    }
-    .typeSection {
-      margin-top: 20px;
-      width: 100%;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-
-      img {
-        margin-left: var(--xl-sizing);
-        width: 75%;
-        height: auto;
-        cursor: pointer;
-        margin-bottom: 20px;
-      }
-    }
-    .colorItem {
-      width: 50px;
-      height: 50px;
-      margin: var(--xs-sizing);
-      border-radius: var(--xs-sizing);
-      position: relative;
-      cursor: pointer;
-      transition: outline 0.3s;
-
-      &.selected {
-        outline: 3px solid white;
-      }
-    }
+    justify-content: center;
+    width: auto;
+    padding: var(--l-sizing);
+    z-index: 10;
+  }
+  .logo {
+    position: absolute;
+    top: 4%;
+    right: 3%;
+    font-size: var(--xl-sizing);
+    color: var(--whiteColor);
   }
 `;
 
 function Home() {
-  const [isRightSideOpen, setIsRightSideOpen] = useState(false);
-  const [selectedColor, setSelectedColor] = useState(null);
-  const colors = ["Blue", "Green", "Red", "Orange", "White", "Black"];
-  const [selectedType, setSelectedType] = useState(null);
-  const [activePanel, setActivePanel] = useState("colors");
   const [pixelStreaming, setPixelStreaming] = useState(null);
-  const [selectLogoColor, setselectLogoColor] = useState(null);
-  const logoColors = ["Blue", "Green", "Red", "Orange", "White", "Black"];
-
-  //
+  const [activeButton, setActiveButton] = useState("Inner");
+  const [animatingButton, setAnimatingButton] = useState("");
+  const [showLoading, setShowLoading] = useState(true);
+  const [showSnackbar, setShowSnackbar] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState("");
 
   const onPixelStreamingReady = (streamingInstance) => {
     setPixelStreaming(streamingInstance);
   };
 
-  //
-
-  const typeImages = [
-    { type: "type1", imagePath: "src/assets/hoodie.png" },
-    { type: "type2", imagePath: "src/assets/tee.png" },
-  ];
-
-  const typeLogo = [
-    { type: "type1", imagePath: "src/assets/cross.png" },
-    { type: "type2", imagePath: "src/assets/labarak.png" },
-    { type: "type3", imagePath: "src/assets/dauphin.png" },
-  ];
-
-  const [selectLogo, setselectLogo] = useState(typeLogo[0].type);
-
-  const handlePanelToggle = (panel) => {
-    setActivePanel(panel);
-  };
-
-  const handleTypeClick = (type, index) => {
-    setSelectedType(type);
-    console.log(index);
+  const handleButtonClick = (buttonId) => {
+    setActiveButton(buttonId);
+    triggerAnimation(buttonId);
 
     if (pixelStreaming) {
-      pixelStreaming.emitUIInteraction({ type: 1, index: index });
+      if (buttonId == "Inner") {
+        pixelStreaming.emitUIInteraction({ Show: "Inner" });
+      }
+      if (buttonId == "Outer") {
+        pixelStreaming.emitUIInteraction({ Show: "Outer" });
+      }
+      if (buttonId == "InnerOuter") {
+        pixelStreaming.emitUIInteraction({ Show: "InnerOuter" });
+      }
     }
   };
 
-  const handleColorClick = (color, index) => {
-    setSelectedColor(color);
-    console.log(index);
-
+  const handleButtonWeather = (buttonId) => {
+    triggerAnimation(buttonId);
     if (pixelStreaming) {
-      pixelStreaming.emitUIInteraction({ type: 0, index: index });
+      pixelStreaming.emitUIInteraction({ Weather: "no rain" });
     }
   };
 
-  const handleLogoClick = (type, index) => {
-    setselectLogo(type);
-    console.log(index);
-
-    if (pixelStreaming) {
-      pixelStreaming.emitUIInteraction({ type: 2, index: index });
-    }
+  const handleButtonExportRef = (buttonId) => {
+    triggerAnimation(buttonId);
+    setSnackbarMessage(`${activeButton} has been added to cart`);
+    setShowSnackbar(true);
+    setTimeout(() => setShowSnackbar(false), 3000);
   };
 
-  const handleLogoColorClick = (type, index) => {
-    setselectLogoColor(type);
-    console.log(index);
+  const triggerAnimation = (buttonId) => {
+    setAnimatingButton(buttonId);
+    setTimeout(() => setAnimatingButton(""), 200);
+  };
 
-    if (pixelStreaming) {
-      pixelStreaming.emitUIInteraction({ type: 3, index: index });
-    }
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+  const loaderOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: loader,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
   };
 
   return (
-    <StyledHome isRightSideOpen={isRightSideOpen}>
-      <div className="streamingSide">
-        <PixelStreamingWrapper
-          initialSettings={{
-            AutoPlayVideo: true,
-            AutoConnect: true,
-            ss: "ws://192.168.10.74:80",
-            StartVideoMuted: true,
-            HoveringMouse: true,
-            WaitForStreamer: true,
-          }}
-          onReady={onPixelStreamingReady}
-        />
-      </div>
-      <button
-        onClick={() => setIsRightSideOpen(!isRightSideOpen)}
-        className="toggleButton"
-      >
-        {isRightSideOpen ? <ChevronRight /> : <ChevronLeft />}
-      </button>
-      <div className="rightSide">
-        <h1>Configurateur</h1>
-        <div className="leftPanel">
-          <button
-            onClick={() => handlePanelToggle("colors")}
-            className={activePanel === "colors" ? "active" : ""}
-          >
-            Couleurs
-          </button>
-          <button
-            onClick={() => handlePanelToggle("types")}
-            className={activePanel === "types" ? "active" : ""}
-          >
-            Type
-          </button>
-          <button
-            onClick={() => handlePanelToggle("logo")}
-            className={activePanel === "logo" ? "active" : ""}
-          >
-            Logo
-          </button>
-          <button
-            onClick={() => handlePanelToggle("logoColor")}
-            className={activePanel === "logoColor" ? "active" : ""}
-          >
-            Logo color
-          </button>
-        </div>
-        {activePanel === "colors" && (
-          <ul className="colorList">
-            {colors.map((color, index) => (
-              <li
-                key={index}
-                className={`colorItem ${
-                  selectedColor === color ? "selected" : ""
-                }`}
-                style={{ backgroundColor: color }}
-                onClick={() => handleColorClick(color, index)}
-              />
-            ))}
-          </ul>
+    <StyledHome>
+      <PixelStreamingWrapper
+        initialSettings={{
+          AutoPlayVideo: true,
+          AutoConnect: true,
+          ss: "ws://192.168.10.67",
+          StartVideoMuted: true,
+          HoveringMouse: true,
+          WaitForStreamer: true,
+        }}
+        onReady={onPixelStreamingReady}
+      />
+
+      <>
+        {showLoading && (
+          <LoadingWrapper>
+            <Lottie options={defaultOptions} height={200} width={200} />
+            <Lottie options={loaderOptions} height={200} width={200} />
+          </LoadingWrapper>
         )}
-        {activePanel === "types" && (
-          <div className="typeSection">
-            {typeImages.map((typeObj, index) => (
-              <img
-                key={typeObj.type}
-                src={typeObj.imagePath}
-                alt={typeObj.type}
-                onClick={() => handleTypeClick(typeObj.type, index)}
-                style={{
-                  border:
-                    selectedType === typeObj.type ? "4px solid white" : "none",
-                }}
-              />
-            ))}
-          </div>
-        )}
-        {activePanel === "logo" && (
-          <div className="typeSection">
-            {typeLogo.map((typeObj, index) => (
-              <img
-                key={typeObj.type}
-                src={typeObj.imagePath}
-                alt={typeObj.type}
-                onClick={() => handleLogoClick(typeObj.type, index)}
-                style={{
-                  border:
-                    selectLogo === typeObj.type ? "2px solid white" : "none",
-                }}
-              />
-            ))}
-          </div>
-        )}
-        {activePanel === "logoColor" && (
-          <ul className="colorList">
-            {logoColors.map((color, index) => (
-              <li
-                key={index}
-                className={`colorItem ${
-                  selectLogoColor === color ? "selected" : ""
-                }`}
-                style={{ backgroundColor: color }}
-                onClick={() => handleLogoColorClick(color, index)}
+
+        {!showLoading && (
+          <>
+            <div
+              onClick={() =>
+                (window.location.href = "https://www.aigle.com/fr/fr")
+              }
+              className="logo"
+            >
+              Aigle
+            </div>
+            {showSnackbar && <Snackbar>{snackbarMessage}</Snackbar>}
+            <div className="buttonContainer">
+              <ToggleButton
+                className={animatingButton === "Inner" ? "animatedOnClick" : ""}
+                active={activeButton === "Inner"}
+                onClick={() => handleButtonClick("Inner")}
               >
-                <span style={{ color: "pink" }}>Logo</span>
-              </li>
-            ))}
-          </ul>
+                Inner
+              </ToggleButton>
+              <ToggleButton
+                className={animatingButton === "Outer" ? "animatedOnClick" : ""}
+                active={activeButton === "Outer"}
+                onClick={() => handleButtonClick("Outer")}
+              >
+                Outer
+              </ToggleButton>
+              <ToggleButton
+                className={
+                  animatingButton === "InnerOuter" ? "animatedOnClick" : ""
+                }
+                active={activeButton === "InnerOuter"}
+                onClick={() => handleButtonClick("InnerOuter")}
+              >
+                Inner and Outer
+              </ToggleButton>
+              <ToggleButton
+                className={
+                  animatingButton === "nextWeather" ? "animatedOnClick" : ""
+                }
+                onClick={() => handleButtonWeather("nextWeather")}
+              >
+                Next Weather
+              </ToggleButton>
+              <ToggleButton
+                className={
+                  animatingButton === "addToCart" ? "animatedOnClick" : ""
+                }
+                onClick={() => handleButtonExportRef("addToCart")}
+              >
+                Add to cart
+              </ToggleButton>
+            </div>
+          </>
         )}
-      </div>
+      </>
     </StyledHome>
   );
 }
